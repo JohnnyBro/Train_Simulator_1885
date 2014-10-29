6de0572465ba6a8889c4655d72f65fb86352401d
Johnny Bravo <jbravozam@gmail.com>
2014-10-28 18:57:53 -0600

    Creacion de botones
    Se han crado los botones de abrir, guardar  y cerrar, asi como se ha
    creado el metodo para implemetar el flujo de datos

--- Ventana.java
+++ Ventana.java
@@ -1,3 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -14,13 +22,14 @@ public class Ventana {
 	JMenuBar menubar= new JMenuBar();
 	JMenu menu= new JMenu("Menu");
 	JMenuItem abrir= new JMenuItem("Abrir");
+	JMenuItem guardar= new JMenuItem("Guardar");
 	JMenuItem salir= new JMenuItem("Salir");
 	JTextArea texto= new JTextArea("");
 	
 	JTextArea textarea= new JTextArea();
 	JLabel nombretext= new JLabel();
 	
-	public Ventana()
+	public  Ventana()
 	{
 		frame.setTitle("Train Simulator 1885");   
 		frame.setSize(1000, 600);
@@ -30,21 +39,110 @@ public class Ventana {
 		frame.add(nombretext);
 		frame.add(menubar);
 		textarea.setVisible(false);
-		textarea.setBounds(10, 50, 700, 500);
+		textarea.setBounds(10, 50, 900, 250);
 		nombretext.setVisible(true);
-		nombretext.setBounds(25, 30, 500, 10);
+		nombretext.setBounds(25, 30, 900, 10);
 
 		
 		menubar.add(menu);
 		menubar.setBounds(0, 0, 1000, 20);
-		menu.add(abrir=new JMenu("Abrir"));
-		menu.add(salir= new JMenu("Salir"));
+		menu.add(abrir=new JMenuItem("Abrir"));
+		menu.add(guardar=new JMenuItem("Guardar"));
+		menu.add(salir= new JMenuItem("Salir"));
 		
 		
-		
-		
+		Abrir open=new Abrir();
+		abrir.addActionListener(open);
+		Guardar save= new Guardar();
+		guardar.addActionListener(save);
+		Salir exit=new Salir();
+		salir.addActionListener(exit);
 		
 	}
- }
- 
+	
+	private class Abrir implements ActionListener
+	{
 
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			
+			JFileChooser seleccionar = new JFileChooser();
+			seleccionar.setCurrentDirectory(new java.io.File("."));
+			seleccionar.setDialogTitle("Directorio");
+			seleccionar.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+			seleccionar.setAcceptAllFileFilterUsed(false);
+			
+			if (seleccionar.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+				//System.out.println("Directorio: " + seleccionar.getCurrentDirectory());
+				nombretext.setText(seleccionar.getSelectedFile()+"");
+				textarea.setVisible(true);		
+				String ruta1="";
+				ruta1=nombretext.getText();
+				
+				String infor="";
+				try{
+					String line;
+					FileInputStream is = new FileInputStream(ruta1);
+					DataInputStream ds =new DataInputStream(is);
+					while((line=ds.readLine())!=null){
+						infor=infor+line+"\n";
+						
+					}
+					ds.close();
+					is.close();
+					}
+				catch(IOException evr){
+					System.out.println("Error de archivo "+evr);
+				}
+				textarea.setText(infor);
+				} else {
+				textarea.setVisible(false);
+		}
+		
+	}
+		
+ }
+	//Metodo Guardar
+	private class Guardar implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			
+			String ruta2;
+			String agregar;
+			agregar=textarea.getText();
+			ruta2=nombretext.getText();
+			try{
+			FileOutputStream os= new FileOutputStream(ruta2);
+			os.write(agregar.getBytes());
+			os.close();
+		   }catch(IOException ev){
+				System.out.println("Error de Archivo "+ ev);
+				
+			}
+			
+		}
+			
+		}
+		
+	
+	//Metodo Salir
+	private class Salir implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			System.exit(0);
+		}
+		
+	}
+
+	/*@Override
+	public void actionPerformed(ActionEvent e) {
+		e = (JButton) e.getSource();
+		
+	}*/
+ 
+}
