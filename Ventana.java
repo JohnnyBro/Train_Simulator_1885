c8ed51c641595ed5573b5afe2f0d8f3b5d7f14c7
Johnny Bravo <jbravozam@gmail.com>
2014-10-28 22:00:44 -0600

    Creacion de barras de desplazamiento

--- Ventana.java
+++ Ventana.java
@@ -5,12 +5,14 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import javax.swing.Box;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 
@@ -18,17 +20,18 @@ import javax.swing.JTextArea;
 
 public class Ventana {
 	JFrame frame= new JFrame();
-	
+
 	JMenuBar menubar= new JMenuBar();
 	JMenu menu= new JMenu("Menu");
 	JMenuItem abrir= new JMenuItem("Abrir");
 	JMenuItem guardar= new JMenuItem("Guardar");
 	JMenuItem salir= new JMenuItem("Salir");
 	JTextArea texto= new JTextArea("");
-	
+	Box cuadro = Box.createHorizontalBox();
+
 	JTextArea textarea= new JTextArea();
 	JLabel nombretext= new JLabel();
-	
+
 	public  Ventana()
 	{
 		frame.setTitle("Train Simulator 1885");   
@@ -38,48 +41,53 @@ public class Ventana {
 		frame.add(textarea);
 		frame.add(nombretext);
 		frame.add(menubar);
+		frame.add(cuadro);
 		textarea.setVisible(false);
-		textarea.setBounds(10, 50, 900, 250);
+		textarea.setBounds(50, 50, 890, 250);
 		nombretext.setVisible(true);
-		nombretext.setBounds(25, 30, 900, 10);
+		nombretext.setBounds(25, 30, 950, 10);
+		cuadro.setBounds(50, 50, 890, 250);
+		cuadro.setVisible(false);
+		cuadro.add(new JScrollPane(textarea));
 
-		
+
 		menubar.add(menu);
 		menubar.setBounds(0, 0, 1000, 20);
 		menu.add(abrir=new JMenuItem("Abrir"));
 		menu.add(guardar=new JMenuItem("Guardar"));
 		menu.add(salir= new JMenuItem("Salir"));
-		
-		
+
+
 		Abrir open=new Abrir();
 		abrir.addActionListener(open);
 		Guardar save= new Guardar();
 		guardar.addActionListener(save);
 		Salir exit=new Salir();
 		salir.addActionListener(exit);
-		
+
 	}
-	
+
 	private class Abrir implements ActionListener
 	{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
-			
+
 			JFileChooser seleccionar = new JFileChooser();
 			seleccionar.setCurrentDirectory(new java.io.File("."));
 			seleccionar.setDialogTitle("Directorio");
 			seleccionar.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 			seleccionar.setAcceptAllFileFilterUsed(false);
-			
+
 			if (seleccionar.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
 				//System.out.println("Directorio: " + seleccionar.getCurrentDirectory());
 				nombretext.setText(seleccionar.getSelectedFile()+"");
+				cuadro.setVisible(true);
 				textarea.setVisible(true);		
 				String ruta1="";
 				ruta1=nombretext.getText();
-				
+
 				String infor="";
 				try{
 					String line;
@@ -87,47 +95,47 @@ public class Ventana {
 					DataInputStream ds =new DataInputStream(is);
 					while((line=ds.readLine())!=null){
 						infor=infor+line+"\n";
-						
+
 					}
 					ds.close();
 					is.close();
-					}
+				}
 				catch(IOException evr){
 					System.out.println("Error de archivo "+evr);
 				}
 				textarea.setText(infor);
-				} else {
+			} else {
 				textarea.setVisible(false);
+			}
+
 		}
-		
+
 	}
-		
- }
 	//Metodo Guardar
 	private class Guardar implements ActionListener
 	{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			
+
 			String ruta2;
 			String agregar;
 			agregar=textarea.getText();
 			ruta2=nombretext.getText();
 			try{
-			FileOutputStream os= new FileOutputStream(ruta2);
-			os.write(agregar.getBytes());
-			os.close();
-		   }catch(IOException ev){
+				FileOutputStream os= new FileOutputStream(ruta2);
+				os.write(agregar.getBytes());
+				os.close();
+			}catch(IOException ev){
 				System.out.println("Error de Archivo "+ ev);
-				
+
 			}
-			
+
 		}
-			
-		}
-		
-	
+
+	}
+
+
 	//Metodo Salir
 	private class Salir implements ActionListener{
 
@@ -136,13 +144,13 @@ public class Ventana {
 			// TODO Auto-generated method stub
 			System.exit(0);
 		}
-		
+
 	}
 
 	/*@Override
 	public void actionPerformed(ActionEvent e) {
 		e = (JButton) e.getSource();
-		
+
 	}*/
- 
+
 }
